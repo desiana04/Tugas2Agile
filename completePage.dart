5dcc9c41968e60863886c009cf1bb9e24bc8134e
desianasitumorang <desianarsitumorang@gmail.com>
2023-07-10 12:16:24 +0700

    menabahkan fitur untuk jika di klik masih kosong

---
+++ completePage.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class CompletePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Text(
+          'Halaman Kosong',
+          style: TextStyle(fontSize: 24),
+        ),
+      ),
+    );
+  }
+}
